3506455b509d643f12ab8460a464873508d1e982
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-28 17:47:37 +0200

    [UPD] added array "elenco-emails"

---
+++ Mail/js/script.js
@@ -0,0 +1,7 @@
+//creo la lista delle email tramite un array
+const elenco_emails = [
+    "iamSatan666@hell.com",
+    "pincopallo67@gmail.com",
+    "tiziocaio25@tiscali.it",
+    "maremmma.maliarda@live.it",
+];
