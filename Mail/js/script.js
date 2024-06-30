413c4f5c3135b340b62304f3dd923e3a006a2e21
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-06-30 17:51:47 +0200

    [UPD] fixed script.js

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,7 +1,31 @@
 //creo la lista delle email tramite un array
 const elenco_emails = [
     "iamSatan666@hell.com",
-    "pincopallo67@gmail.com",
+    "pincopallo77@gmail.com",
     "tiziocaio25@tiscali.it",
-    "maremmma.maliarda@live.it",
+    "maremmma.maliarda49@live.it",
 ];
+
+//Inserisco la variabile di controllo, detta flag
+const btn = document.getElementById("search").value.toLowerCase();
+let found_email = false;
+
+btn.addEventListener("click", function () {
+
+    //Recupero il valor inserito nel campo input
+    let word = document.getElementById("e-mail_box").value;
+   
+    //Mi assicuro che l'elenco delle e-mails scritte sopra siano associato ad un valore true affinché il bottone search le trovi
+    for (let i = 0; i < elenco_emails.length; i++) {
+        if (elenco_emails[i].toLowerCase() === word) {
+            found_email = true;
+        }
+    }
+
+    if (found_email === true) {
+        console.log("La tua e_mail è presente nel nostro database")
+    }
+    else {
+        console.log("Spiacente, la tua e-mail non è presente nel nostro database")
+    }
+})
